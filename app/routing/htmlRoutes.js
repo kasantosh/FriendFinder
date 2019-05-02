2d104cf387fa0cbf4407d573c8c6fba0f897ed37
Santosh Kumar <kasantosh@yahoo.com>
2019-05-02 14:29:00 -0400

    linked server and apiRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,6 @@
 var path = require("path");
 
 module.exports = function(app) {
-  app.use(express.static(path.join(__dirname, "public")));
   app.get("/", function(req, res) {
     res.sendFile("/home.html");
   });
