258bfdc9d6003ff1b73a9f927f2d619d32cfd751
Santosh Kumar <kasantosh@yahoo.com>
2019-05-03 13:18:26 -0400

    Made a friends file with some data for testing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
-var path = require("path");
+const path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile("/home.html");
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   app.get("/survey", function(req, res) {
-    res.sendFile("/survey.html");
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+require("./app/routing/htmlRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
   console.log(`Listening on port: ${PORT}`);
