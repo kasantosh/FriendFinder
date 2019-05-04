1bc0cfa27da8610445af4a71673a7aad6567d391
Santosh Kumar <kasantosh@yahoo.com>
2019-05-04 15:03:24 -0400

    Created UI for home page

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 require("./app/routing/htmlRoutes")(app);
 
 require("./app/routing/apiRoutes")(app);
